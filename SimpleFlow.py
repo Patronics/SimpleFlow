9409efbb8799480ee90ebe3473093bcf7dbff3c1
Patrick Leiser <patrick27leiser@yahoo.com>
2023-03-24 14:24:13 -0700

    add the ability to loop a finite number of times with goto counter

--- SimpleFlow.py
+++ SimpleFlow.py
@@ -4,13 +4,15 @@ def parse_line(line):
     line = line.strip()
     if not line or line.startswith('?'):
         return None
-    tokens = line.split(None, 1)
+    tokens = line.split(None, 2)
     if len(tokens) == 1:
         if line.endswith(':'):
-            return ('label', line[:-1])
+            return ('label', line[:-1], None)
         else:
-            return (tokens[0], None)
+            return (tokens[0], None, None)
     elif len(tokens) == 2:
+        return tokens[0], tokens[1], None
+    elif len(tokens) == 3:
         return tokens
     else:
         raise ValueError(f"Invalid line: {line}")
@@ -23,9 +25,10 @@ def parse_program(program):
 
 def run_program(commands):
     goto_labels = {cmd[1]: idx for idx, cmd in enumerate(commands) if cmd[0] == 'label'}
+    loop_counters = {}
     idx = 0
     while idx < len(commands):
-        cmd, arg = commands[idx]
+        cmd, arg, loop_arg = commands[idx]
         if cmd == "pause":
             time.sleep(float(arg))
         elif cmd == "print":
@@ -33,8 +36,18 @@ def run_program(commands):
         elif cmd == "goto":
             if arg not in goto_labels:
                 raise ValueError(f"Invalid goto label: {arg}")
-            idx = goto_labels[arg]
-            continue
+            if loop_arg:
+                max_loops = int(loop_arg)
+                if arg not in loop_counters:
+                    loop_counters[arg] = 1
+                else:
+                    loop_counters[arg] += 1
+                if loop_counters[arg] <= max_loops:
+                    idx = goto_labels[arg]
+                    continue
+            else:
+                idx = goto_labels[arg]
+                continue
         elif cmd == "label":
             pass  # Ignore label lines
         else:
@@ -43,14 +56,18 @@ def run_program(commands):
 
 def main():
     program = """
-        ? This is a simple example program
-        print Hello
+	? This is a simple example program
+        print Start
         pause 1
-        print World
+        ? Program loop here
+        loop:
+        print InsideLoop
+        pause 0.5
+        goto loop 3
+        ? Label for end of loop
+        end_of_loop:
+        print AfterLoop
         pause 1
-        goto end
-        print NotExecuted
-        ? Program ends here
         goto end
         ? Label for program end
         end:
